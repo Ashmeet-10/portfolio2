7725133010b864460da152a22afdb7967009c17a
Ashmeet Singh <ashmeet2846@gmail.com>
2024-03-11 15:58:28 +0530

    1st commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,23 @@
+import Navbar from './Navbar'
+
+import Footer from './Footer'
+
+export default function Contact() {
+  return (
+
+        <div>
+        <Navbar />
+        <div className="contact">
+        <h4 className='icons light-colour-navbar'> Get in touch with me through:</h4>
+        <h5 className="connect"><i className="fa-brands fa-instagram fa-xl zoom" style={{ color: '#707275' }}> </i> <a href="https://www.instagram.com/liveleenn" className='connect'> Instagram </a><br /> </h5>
+        <h5 className="connect">  <i className="fa-brands fa-github fa-xl zoom" style={{ color: '#707275' }} > </i> <a href="https://github.com/Live-25" className='connect'> Github</a> <br /> </h5>
+        <h5 className="connect">  <i className="fa-brands fa-linkedin-in fa-xl zoom" style={{ color: '#707275' }}> </i><a href="https://www.linkedin.com/in/liveleen-kaur-4b4033248" className='connect'>  LinkedIn</a> <br /></h5>
+        <h5 className="connect">  <i className="fa-solid fa-envelope fa-xl zoom" style={{ color: '#707275' }}> </i> <a href="mailto:liveleen25@gmail.com" className='connect'> liveleen25@gmail.com</a> <br /></h5>
+        </div>
+        <Footer />
+        </div> 
+  
+  )
+}
+
+/* SLIGHT ISSUE */
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+export default function Navbar() {
+  const [isToggled, setToggle] = useState(false)
+
+  const toggleButton = () => {
+    setToggle(!isToggled)
+  }
+
+  return (
+    <div className='navbar-stick'>
+      <div>
+        <nav className='navbar navbar-expand-lg'>
+          <div className='container-fluid'>
+            <a className='navbar-brand zoom logo-font-LK' href='/'>
+              {' '}
+              L K <h6 className='logo'> Liveleen Kaur</h6>{' '}
+            </a>
+            <button
+              onClick={toggleButton}
+              className='navbar-toggler'
+              type='button'
+              data-bs-toggle='collapse'
+              aria-controls='navbarNav'
+              aria-expanded='false'
+              aria-label='Toggle navigation'
+            >
+              <span className='navbar-toggler-icon light'></span>
+            </button>
+            <div
+              className={
+                'collapse navbar-collapse' + (isToggled ? ' show' : '')
+              }
+              id='navbarNav'
+            >
+              <ul className='navbar-nav'>
+                <li className='nav-item'>
+                  <a className='nav-link zoom' href='/about'>
+                    About
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link zoom' href='/technology'>
+                    Skills
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link zoom' href='/projects'>
+                    Projects
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link zoom' href='/contact'>
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <hr className='orange' />
+      </div>
+    </div>
+  )
+}
